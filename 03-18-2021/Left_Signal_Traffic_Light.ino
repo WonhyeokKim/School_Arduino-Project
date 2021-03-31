556d900136ac3138378b90f44060d29c4b404855
Project_TL <kimpiao020@gmail.com>
2021-04-01 08:38:47 +0900

    Update: add 19 day code

--- 03-18-2021/Left_Signal_Traffic_Light.ino
+++ 03-18-2021/Left_Signal_Traffic_Light.ino
@@ -8,23 +8,28 @@ void setup() {
   pinMode(yellowLed, OUTPUT);
   pinMode(leftLed, OUTPUT);
   pinMode(greenLed, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
   digitalWrite(greenLed, HIGH);
+  Serial.println("Green Light");
   delay(1000 * 2);
   digitalWrite(greenLed, LOW);
   
   digitalWrite(yellowLed, HIGH);
+  Serial.println("Green Light");
   delay(1000 * 1);
   digitalWrite(yellowLed, LOW);
   
   digitalWrite(redLed, HIGH);
   digitalWrite(leftLed, HIGH);
+  Serial.println("Red, Left Light");
   delay(1000 * 1);
   digitalWrite(leftLed, LOW);
   
   digitalWrite(yellowLed, HIGH);
+  Serial.println("Yellow Light");
   delay(1000 * 1);
   digitalWrite(yellowLed, LOW);
   
