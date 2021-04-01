774ff911ed29ecd2a14db1ba8084bfde1a90dce6
Project_TL <kimpiao020@gmail.com>
2021-04-01 10:00:14 +0900

    Update: Add today source code.

--- 04-01-2021/main.ino
+++ 04-01-2021/main.ino
@@ -1,8 +1,33 @@
+int setRed = 11;
+int setBlue = 10;
+int setGreen = 9;
+
 void setup() {
+    pinMode(setRed, OUTPUT);
+    pinMode(setBlue, OUTPUT);
+    pinMode(setGreen, OUTPUT);
+
     Serial.begin(9600);
 }
 
 void loop() {
+    analogWrite(setRed, 255);
+    analogWrite(setBlue, 0);
+    analogWrite(setGreen, 0);
+
+    delay(500);
+
+    analogWrite(setRed, 0);
+    analogWrite(setBlue, 255);
+    analogWrite(setGreen, 0);
+
+    delay(500);
+
+    analogWrite(setRed, 0);
+    analogWrite(setBlue, 0);
+    analogWrite(setGreen, 255);
+
+    delay(500);
 }
 
 void sleepSecond(int timing) {
