47641c555c84a834ccffb6805b88708407e38069
Project_TL <kimpiao020@gmail.com>
2021-04-01 10:29:02 +0900

    Update: add project

---
+++ 04-01-2021/main2.ino
@@ -0,0 +1,31 @@
+int setRed = 11;
+int setBlue = 10;
+int setGreen = 9;
+
+void setup() {
+    pinMode(setRed, OUTPUT);
+    pinMode(setBlue, OUTPUT);
+    pinMode(setGreen, OUTPUT);
+
+    Serial.begin(9600);
+}
+
+void loop() {
+    analogWrite(setRed, 255);
+    analogWrite(setBlue, 0);
+    analogWrite(setGreen, 255);
+
+    delay(500); // #FFFF00 RGB기준
+
+    analogWrite(setRed, 255);
+    analogWrite(setBlue, 255);
+    analogWrite(setGreen, 0);
+
+    delay(500); // #FF00FF RGB기준
+
+    analogWrite(setRed, 0);
+    analogWrite(setBlue, 255);
+    analogWrite(setGreen, 255);
+
+    delay(500); / ##00FFFF RGB기준
+}
