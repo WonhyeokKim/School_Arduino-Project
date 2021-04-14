4d64635e3922ff2be24169772cc5f1ff66d099f3
ProjectTL12345 <kimpiao020@gmail.com>
2021-04-15 08:55:44 +0900

    Done!

--- April/04-15-2021/main.ino
+++ April/04-15-2021/main.ino
@@ -1,8 +1,20 @@
+int button = 13;
+int led = 12;
 
 void setup() {
+    pinMode(button, INPUT);
+    pinMode(led, OUTPUT);
+
     Serial.begin(9600);
 }
 
 void loop() {
-    // TODO
+    int readValue = digitalRead(button);
+  	Serial.println(readValue);
+  
+    if (readValue == HIGH) {
+        digitalWrite(led, HIGH);
+    } else {
+        digitalWrite(led, LOW);
+    }
 }
