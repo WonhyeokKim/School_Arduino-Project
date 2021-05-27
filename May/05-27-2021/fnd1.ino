17101498d8f6e1d57e392e51ff2a53dce8386e77
Project_TL <projecttl12345@gmail.com>
2021-05-27 10:25:48 +0900

    Update fnd1.ino

--- May/05-27-2021/fnd1.ino
+++ May/05-27-2021/fnd1.ino
@@ -34,6 +34,7 @@ void setup() {
 }
 
 void loop() {
+  // 0
   digitalWrite(pinDP, HIGH);
   
   digitalWrite(pinA, LOW);
@@ -46,6 +47,7 @@ void loop() {
   
   delay(1000);
   
+  // 1
   digitalWrite(pinA, HIGH);
   digitalWrite(pinB, LOW);
   digitalWrite(pinC, LOW);
@@ -56,6 +58,7 @@ void loop() {
   
   delay(1000);
   
+  // 2
   digitalWrite(pinA, LOW);
   digitalWrite(pinB, LOW);
   digitalWrite(pinC, HIGH);
@@ -66,6 +69,7 @@ void loop() {
   
   delay(1000);
   
+  // 3
   digitalWrite(pinA, LOW);
   digitalWrite(pinB, LOW);
   digitalWrite(pinC, LOW);
@@ -76,6 +80,7 @@ void loop() {
   
   delay(1000);
   
+  // 4
   digitalWrite(pinA, HIGH);
   digitalWrite(pinB, LOW);
   digitalWrite(pinC, LOW);
@@ -86,6 +91,7 @@ void loop() {
   
   delay(1000);
   
+  // 5
   digitalWrite(pinA, LOW);
   digitalWrite(pinB, HIGH);
   digitalWrite(pinC, LOW);
@@ -96,6 +102,7 @@ void loop() {
   
   delay(1000);
   
+  // 6
   digitalWrite(pinA, LOW);
   digitalWrite(pinB, HIGH);
   digitalWrite(pinC, LOW);
@@ -106,6 +113,7 @@ void loop() {
   
   delay(1000);
   
+  // 7
   digitalWrite(pinA, LOW);
   digitalWrite(pinB, LOW);
   digitalWrite(pinC, LOW);
@@ -116,6 +124,7 @@ void loop() {
   
   delay(1000);
   
+  // 8
   digitalWrite(pinA, LOW);
   digitalWrite(pinB, LOW);
   digitalWrite(pinC, LOW);
@@ -126,6 +135,7 @@ void loop() {
   
   delay(1000);
   
+  // 9
   digitalWrite(pinA, LOW);
   digitalWrite(pinB, LOW);
   digitalWrite(pinC, LOW);
@@ -136,102 +146,3 @@ void loop() {
   
   delay(1000);
 }
-/* Convert this later
-int printNumber(int type) {
-  switch (type) {
-    case 0:
-    segmentOutput(true, true, true, true, true, true, false, false);
-    
-    case 1:
-    segmentOutput(false, true, true, false, false, false, false, false);
-    
-    case 2:
-    segmentOutput(true, true, false, true, true, false, false, false);
-    
-    case 3:
-    segmentOutput(true, true, true, true, false, false, true, false);
-    
-    case 4:
-    segmentOutput(false, true, true, false, false, true, true, false);
-    
-    case 5:
-    segmentOutput(true, false, true, true, false, true, true, false);
-    
-    case 6:
-    segmentOutput(true, false, true, true, true, true, true, false);
-    
-    case 7:
-    segmentOutput(true, true, true, false, false, false, false, false);
-    
-    case 8:
-    segmentOutput(true, true, true, true, true, true, true, false);
-    
-    case 9:
-    segmentOutput(true, true, true, false, false, true, true, false);
-    case 10:
-    segmentOutput(true, true, true, true, true, true, false, false);
-    
-    case 11:
-    segmentOutput(true, true, true, true, true, true, false, false);
-    
-    case 12:
-    segmentOutput(true, true, true, true, true, true, false, false);
-    
-    case 13:
-    segmentOutput(true, true, true, true, true, true, false, false);
-    
-    case 14:
-    segmentOutput(true, true, true, true, true, true, false, false);
-    
-    case 15:
-    segmentOutput(true, true, true, true, true, true, false, false);
-  }
-  
-  return type;
-}
-
-void segmentOutput(
-  bool enableA,
-  bool enableB,
-  bool enableC,
-  bool enableD,
-  bool enableE,
-  bool enableF,
-  bool enableG,
-  bool enableDP
-) {
-  if (enableA) {
-  	digitalWrite(pinA, HIGH);
-  } else if (enableB) {
-    digitalWrite(pinB, HIGH);
-  } else if (enableC) {
-    digitalWrite(pinC, HIGH);
-  } else if (enableD) {
-    digitalWrite(pinD, HIGH);
-  } else if (enableE) {
-    digitalWrite(pinE, HIGH);
-  } else if (enableF) {
-    digitalWrite(pinF, HIGH);
-  } else if (enableG) {
-    digitalWrite(pinG, HIGH);
-  } else if (enableDP) {
-    digitalWrite(pinDP, HIGH);
-  } else if (!enableA) {
-  	digitalWrite(pinA, LOW);
-  } else if (!enableB) {
-    digitalWrite(pinB, LOW);
-  } else if (!enableC) {
-    digitalWrite(pinC, LOW);
-  } else if (!enableD) {
-    digitalWrite(pinD, LOW);
-  } else if (!enableE) {
-    digitalWrite(pinE, LOW);
-  } else if (!enableF) {
-    digitalWrite(pinF, LOW);
-  } else if (!enableG) {
-    digitalWrite(pinG, LOW);
-  } else if (!enableDP) {
-    digitalWrite(pinDP, LOW);
-  }
-}
-*/
