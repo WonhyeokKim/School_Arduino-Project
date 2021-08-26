06fd8c97735456362133015f0048b4bbe70b63ae
ProjectTL12345 <projecttl12345@gmail.com>
2021-08-26 10:24:06 +0900

    done

--- August/2021-08-26/project1/main/main.ino
+++ August/2021-08-26/project1/main/main.ino
@@ -1,17 +1,18 @@
 #include <led_control.h>
 
 // 20504 Wonhyeok Kim
-LedControl led;
+
+void setup() {}
 
 void loop() {
-  for (int i = 6; i > 13; i++) {
-    led = LedControl(i);
+  for (int i = 12; i > 6; i--) {
+    LedControl led = LedControl(i);
     led.led_on();
     delay(500);
   }
 
-  for (int i = 6; i > 13; i++) {
-    led = LedControl(i);
+  for (int i = 12; i > 6; i--) {
+    LedControl led = LedControl(i);
     led.led_off();
   }
 
